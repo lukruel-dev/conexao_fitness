5d6a94613e5a2d1c49b45ff9c602f3ea7c8c10de
lukruel-dev <lkbenq@gmail.com>
2026-08-05 19:52:57 -0300

    fix(frontend): configure capacitor keyboard plugin to properly resize webview on android

--- conexao-fitness-frontend/capacitor.config.ts
+++ conexao-fitness-frontend/capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.conexaofitness.app',
   appName: 'Conexão Fitness',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    Keyboard: {
+      resize: 'body',
+      resizeOnFullScreen: true,
+    }
+  }
 };
 
 export default config;
